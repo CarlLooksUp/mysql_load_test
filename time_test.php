431bf048f68f16225adda9cae4179c5244caca8c
Carl Morrissey <carl.morrissey@gmail.com>
2016-09-02 08:55:25 -0400

    mock data and initial memory runs

--- time_test.php
+++ time_test.php
@@ -3,7 +3,7 @@
 require_once('vendor/fzaninotto/faker/src/autoload.php');
 
 //get connection details
-$settings = parse_ini_file('db_cxn.ini', true);
+$settings = parse_ini_file('db_cxn.ini', false);
 $dsn = "{$settings['driver']}:dbname={$settings['db_name']};host={$settings['host']}";
 $user = $settings['username'];
 $password = $settings['password'];
@@ -11,13 +11,35 @@ $password = $settings['password'];
 $cxn = new PDO($dsn, $user, $password);
 $faker = Faker\Factory::create();
 
+$a = build_test_data(1000000, $faker);
+echo memory_get_usage() . "\n";
+echo count($a) . "\n";
 
+//write to csv
+$start = microtime(true);
+$f = fopen('data/products.csv', 'w');
+foreach($a as $idx => $product) {
+	fputcsv($f, array_values($product));
+}
+$duration = microtime(true) - $start;
 
-function build_test_data($num_rows) {
+echo "Writing to CSV takes $duration s\n";
+
+function build_test_data($num_rows, $faker) {
 	//determine how to handle format
 	$rows = array();
 	for($i = 0; $i < $num_rows; $i++) {
 		$row = array();
-
+		$row['product_id'] = $faker->unique()->randomNumber(8);
+		$row['product_name'] = $faker->words(2, true);
+		$row['brand_name'] = $faker->word;
+		$row['upc'] = $faker->isbn13;
+		$row['qty'] = $faker->randomNumber(2);
+		$row['cost'] = $faker->randomFloat(2, 0, 1000);
+		$row['price'] = $faker->randomFloat(2, 0, 1000);
+		$row['sku'] = $faker->isbn10;
+		$row['mpn'] = $faker->isbn10;
+		$rows[$row['product_id']] = $row;
 	}
+	return $rows;
 }
